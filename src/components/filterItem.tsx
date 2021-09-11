11f2b37d3b534e80039d5372539c27cd75579f61
Mohammad Oumeir Rengony <oumeir.rengony@hangarww.com>
2021-09-11 15:35:37 +0400

    Added filter functionalities

--- src/components/filterItem.tsx
+++ src/components/filterItem.tsx
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { setLocalstorageItem } from "../helper/localstorage";
 import { IfilterItemProps, filterValues } from "./todo";
 
 const FilterItem: React.FC<IfilterItemProps> = ({
   itemsLeft,
   setTodoItems,
+  activeFilter,
+  setActiveFilter,
 }) => {
+  //State
+  const [isDropdownActive, setActiveDropdown] = useState<boolean>(false);
+
+  const dropdownClickHandler = () => {
+    setActiveDropdown(!isDropdownActive);
+  };
+
+  //set the current filter option chosen by user
+  const filterClickHandler = (e: React.MouseEvent<HTMLLIElement>) => {
+    const value = e.currentTarget.innerText;
+    setActiveFilter(filterValues[value as filterValues]);
+    setActiveDropdown(!isDropdownActive);
+  };
+
+  //remove all todo items
+  const clearAll = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setLocalstorageItem([]);
+    setTodoItems([]);
+  };
+
   //generate filter dropdown dynamically
   const displayFilterList = () => {
     const array = [];
     for (const value in filterValues) {
       array.push(
-        <li className="select-box__option" key={value}>
+        <li
+          className="select-box__option"
+          key={value}
+          onClick={filterClickHandler}
+        >
           {value}
         </li>
       );
@@ -24,18 +52,20 @@ const FilterItem: React.FC<IfilterItemProps> = ({
       <p className="remaining-items">{itemsLeft} items left</p>
 
       <div className="select-box">
-        <div className="select-box__current">
-          <span className="select-box__current-value">All</span>
+        <div className="select-box__current" onClick={dropdownClickHandler}>
+          <span className="select-box__current-value">{activeFilter}</span>
           <img
             className="select-box__icon"
             src="http://cdn.onlinewebfonts.com/svg/img_295694.svg"
             alt="Arrow Icon"
           />
         </div>
-        <ul className="select-box__list">{displayFilterList()}</ul>
+        <ul className={`select-box__list ${isDropdownActive ? "show" : ""}`}>
+          {displayFilterList()}
+        </ul>
       </div>
 
-      <div className="remove-item">
+      <div className="remove-item" onClick={clearAll}>
         <p>
           Clear
           <span>
@@ -57,7 +87,7 @@ const StyledFilter = styled.div`
   border-bottom: 1px solid #ededed;
 
   .select-box {
-    width: 80px;
+    width: 83px;
     position: relative;
     color: #60666d;
     text-align: center;
@@ -83,12 +113,13 @@ const StyledFilter = styled.div`
       list-style: none;
       display: none;
       background: #f5f5f5;
+      text-align:start;
       z-index: 1;
-      box-shadow: 0 15px 30px -10px rgba(0, 0, 0, 0.1);
 
       &.show {
         display: block;
       }
+      box-shadow: 0 15px 30px -10px rgba(0, 0, 0, 0.1);
 
       .select-box__option {
         display: block;
--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -4,9 +4,8 @@ import {
   getLocalstorageItem,
   setLocalstorageItem,
 } from "../helper/localstorage";
-import TodoItem from "./todoItem";
 import FilterItem from "./filterItem";
-
+import TodoItem from "./todoItem";
 
 export enum todoStatus {
   Active = "Active",
@@ -34,6 +33,8 @@ export interface ItodoItemProps {
 export interface IfilterItemProps {
   itemsLeft: number;
   setTodoItems: React.Dispatch<React.SetStateAction<ItodoItemsObject[]>>;
+  activeFilter: filterValues;
+  setActiveFilter: React.Dispatch<React.SetStateAction<filterValues>>;
 }
 
 const Todo: React.FC = () => {
@@ -42,6 +43,9 @@ const Todo: React.FC = () => {
     getLocalstorageItem()
   );
 
+  const [activeFilter, setActiveFilter] = useState<filterValues>(
+    filterValues.All
+  );
   const [inputTodo, setInputTodo] = useState<string>("");
 
   const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -62,6 +66,17 @@ const Todo: React.FC = () => {
     setInputTodo("");
   };
 
+  //filter todoItems based on current filter option chosen by user
+  const filterTodoItem = () => {
+    //if activefilter=All return original todoItems array
+    if (activeFilter === filterValues.All) {
+      return todoItems;
+    }
+
+    //else return todoItems basd on active or completed status
+    return todoItems.filter((item) => item.status.toString() === activeFilter);
+  };
+
   return (
     <TodoForm onSubmit={formSubmitHandler}>
       <input
@@ -76,11 +91,13 @@ const Todo: React.FC = () => {
           todoItems.filter((item) => item.status === todoStatus.Active).length
         }
         setTodoItems={setTodoItems}
+        activeFilter={activeFilter}
+        setActiveFilter={setActiveFilter}
       />
 
       <div className="todo-list">
         <ul>
-          {todoItems.map((item, index) => (
+          {filterTodoItem().map((item, index) => (
             <TodoItem
               key={index}
               lastItem={index === todoItems.length - 1}
@@ -98,6 +115,20 @@ const TodoForm = styled.form`
   width: 90%;
   margin: 12px auto;
   background: #fff;
+
+  .info-section {
+    width: 100%;
+    font-size: 14px;
+    padding: 8px 16px;
+
+    .remaining-items {
+      display: inline-block;
+    }
+
+    .filter {
+      display: inline-flex;
+    }
+  }
 
   .new-todo {
     width: 100%;
